a37c22b871349a2574a45af942e356834048a6d0
Armesh <armesh-singh@hotmail.com>
2026-02-10 05:33:59 +0800

    Create config.py with configuration constants and geoname mappings

---
+++ config.py
@@ -0,0 +1,12 @@
+# Configuration Constants
+
+# Geoname mappings
+geonames_mapping = {
+    'CityA': 'geoname_id_1',
+    'CityB': 'geoname_id_2',
+    # Add more mappings here
+}
+
+# Other configuration constants
+API_KEY = 'your_api_key_here'
+TIMEZONE = 'UTC'
